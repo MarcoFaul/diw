0cc54c430ab41712ae276eabeaaf114123ec81e1
marcofaul <marcofaul@protonmail.com>
2020-11-16 18:18:27 +0100

    Change to own version implementation

--- src/Commands/UpdateCommand.php
+++ src/Commands/UpdateCommand.php
@@ -22,9 +22,10 @@ class UpdateCommand implements CommandInterface
         $app->command('self-update', function (InputInterface $input, OutputInterface $output) {
             $io = new SymfonyStyle($input, $output);
 
+            $currentVersion = version()->getVersion();
             $response = Request::get(UpdateCommand::GITHUB_LATEST_RELEASE_URL)->send();
 
-            $isLatestVersion = \version_compare(version()->getVersion(), $response->body->tag_name, '>=');
+            $isLatestVersion = \version_compare($currentVersion, $response->body->tag_name, '>=');
 
             if ($isLatestVersion === true) {
                 $io->writeln('You are on the latest version: ' . $response->body->tag_name);
@@ -32,12 +33,14 @@ class UpdateCommand implements CommandInterface
             }
 
             $helper = $this->getHelperSet()->get('question');
-            $question = new ConfirmationQuestion('Wonna update?', false);
+            $question = new ConfirmationQuestion('Wonna update?', false, '/^(y|j|yes|yeah|jo|yes|jupp)/i');
 
             if ($helper->ask($input, $output, $question)) {
                 $io->writeln('Updating...');
 
                 quietly('brew upgrade ' . \strtolower(APP_NAME));
+
+                $io->success(\sprintf('Upgrade diw from "%s" to latest version "%s"', $currentVersion, version()->getVersion()));
             }
 
         })->descriptions(\sprintf('Update %s Utility', \strtolower(APP_NAME)));
