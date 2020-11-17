e9d5a7f80c2bcb004b5390a83168196972da50fc
marcofaul <marcofaul@protonmail.com>
2020-11-17 16:44:50 +0100

    Removed brew compatibility check

--- src/_includes/Compatibility.php
+++ src/_includes/Compatibility.php
@@ -6,7 +6,7 @@ $dir = __DIR__;
  */
 $inTestingEnvironment = \strpos($_SERVER['SCRIPT_NAME'], 'phpunit') !== false;
 
-if (PHP_OS !== 'Darwin' && ! $inTestingEnvironment) {
+if (PHP_OS !== 'Darwin' && !$inTestingEnvironment) {
     error(APP_NAME . ' only supports the Mac operating system.' . PHP_EOL);
 
     exit(1);
@@ -21,8 +21,9 @@ if (PHP_VERSION_ID <= 70200) {
     exit(1);
 }
 
-if (!$inTestingEnvironment && run('which brew') === '') {
-    error(APP_NAME . ' requires Homebrew to be installed on your Mac.');
-
-    exit(1);
-}
+# doesn't make sense to check for brew when we installed it via brew
+#if (!$inTestingEnvironment && run('which brew') === '') {
+#    error(APP_NAME . ' requires Homebrew to be installed on your Mac.');
+#
+#    exit(1);
+#}
