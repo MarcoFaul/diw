2fbc6a8b2b8e023b4343c64602796179d04eac8a
marcofaul <marcofaul@protonmail.com>
2020-11-23 18:33:10 +0100

    added ssh-key command

---
+++ src/Commands/SshKeyCommand.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace DIW\Commands;
+
+
+use Silly\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @copyright 2020 dasistweb GmbH (https://www.dasistweb.de)
+ */
+class SshKeyCommand implements CommandInterface
+{
+    private const PUB_KEY_PATH = '~/.ssh/id_rsa.pub';
+
+    public static function command(Application $app): void
+    {
+        $app->command('ssh-key', function (InputInterface $input, OutputInterface $output) {
+            $io = new SymfonyStyle($input, $output);
+
+//            if (!file_exists(SshKeyCommand::PUB_KEY_PATH)) {
+//                $io->error('Could not find file ' . SshKeyCommand::PUB_KEY_PATH);
+//                return;
+//            }
+
+            passthruCommand('pbcopy < ' . SshKeyCommand::PUB_KEY_PATH);
+
+            $io->success('Copied ssh key to your clipboard. Use command + c to paste the content somewhere.');
+        })->descriptions('Copy ssh key to your clipboard');
+    }
+}
