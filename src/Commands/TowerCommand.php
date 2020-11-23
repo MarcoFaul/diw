fc733d9a08c5225e67b0cfac9897d11129039baf
marcofaul <marcofaul@protonmail.com>
2020-11-23 16:42:20 +0100

    added git tower command

---
+++ src/Commands/TowerCommand.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace DIW\Commands;
+
+
+use Httpful\Request;
+use Silly\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @copyright 2020 dasistweb GmbH (https://www.dasistweb.de)
+ */
+class TowerCommand implements CommandInterface
+{
+
+    public static function command(Application $app): void
+    {
+        $app->command('tower', function (InputInterface $input, OutputInterface $output) {
+            $io = new SymfonyStyle($input, $output);
+            $filesystem = new Filesystem();
+
+            $io->writeln('Opening git tower');
+            if (!$filesystem->exists('/Applications/Tower.app/Contents/MacOS/gittower')) {
+                $io->error('tower command not found. Please install git tower by following the instructions provided here: https://www.git-tower.com/help/mac/integration/cli-tool');
+                return;
+            }
+
+            $output = runAsUser('/Applications/Tower.app/Contents/MacOS/gittower $(git rev-parse --show-toplevel)');
+
+            if (\strpos($output, 'fatal: Not a git repository') !== false) {
+                $io->error('Could not find git directory');
+            }
+
+        })->descriptions('Open closest git project in Tower');
+    }
+}
